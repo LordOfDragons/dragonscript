1bc154197c695d5ee68e4e9c9216bfaf594f21e3
Plüss Roland <roland@rptd.ch>
2025-07-07 18:09:35 +0200

    wasm fixes

---
+++ site_scons/site_tools/crosscompile/emscripten.py
@@ -0,0 +1,51 @@
+# -*- coding: utf-8 -*-
+#
+# DragonScript
+#
+# Copyright (C) 2025, Plüss Roland ( roland@rptd.ch )
+#
+# This program is free software; you can redistribute it and/or 
+# modify it under the terms of the GNU General Public License 
+# as published by the Free Software Foundation; either 
+# version 2 of the License, or (at your option) any later 
+# version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+#
+
+import os
+
+def generate(env, configGroup):
+	try:
+		env.Tool('emscripten', toolpath=[os.environ['EMSCRIPTEN_TOOL_PATH']])
+	except Exception as e:
+		print('EMScripten missing. Build using "emscons scons ..."')
+		raise e
+	
+	env.Append(CPPFLAGS = ['-DWEB_WASM'])
+	env.Append(CPPFLAGS = ['-O3'])
+	
+	# disable some nag warnings which are plain out stupid
+	#env.Append(CPPFLAGS = ['-Wno-nontrivial-memcall'])
+	
+	env.Append(LINKFLAGS = ['-s', 'SIDE_MODULE=1'])
+	
+	env['CROSSCOMPILE_CFLAGS'] = []
+	env['CROSSCOMPILE_CPPFLAGS'] = []
+	env['CROSSCOMPILE_CXXFLAGS'] = []
+	env['CROSSCOMPILE_LINKFLAGS'] = []
+	
+	env.Append(CFLAGS = env['CROSSCOMPILE_CFLAGS'])
+	env.Append(CPPFLAGS = env['CROSSCOMPILE_CPPFLAGS'])
+	env.Append(CXXFLAGS = env['CROSSCOMPILE_CXXFLAGS'])
+	env.Append(LINKFLAGS = env['CROSSCOMPILE_LINKFLAGS'])
+
+def exists(env):
+	return True
